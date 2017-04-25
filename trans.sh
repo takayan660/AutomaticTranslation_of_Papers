e85b6172151e48e50bc60667be752899f10566e6
TakaharuNakajima <s1526084PQ@s.chibakoudai.jp>
2017-04-25 14:57:10 +0900

    文の整形の部分の改良

--- trans.sh
+++ trans.sh
@@ -1,51 +1,51 @@
 #!/bin/bash
 
-if [ -z $1 ]; then
+if [[ -z $1 ]]; then
     echo 'no'
     exit 1
 fi
 
-if [ -z $2 ]; then
-    filename=$(echo $1 | sed 's/\.pdf//')
-elif [ -n $2 ]; then
-    filename=$2
+if [[ -z $2 ]]; then
+    FILENAME=$(echo $1 | sed 's/\.pdf//')
+elif [[ -n $2 ]]; then
+    FILENAME=$2
 fi
 
 #set -eu
 
-pdfname=$1
-language=ja
+PDFNAME=$1
+FOLDERNAME=$(echo $1 | sed 's/\.pdf//')
+LANGUAGE=ja
 
 atexit() {
-    [[ -n ${tmpfile}* ]] && rm -f "${tmpfile}*"
+    [[ -n ${TMPFILE}* ]] && rm -f "${TMPFILE}*"
 }
-tmpfile1=`mktemp`
-tmpfile2=`mktemp`
+TMPFILE1=`mktemp`
+TMPFILE2=`mktemp`
 trap atexit EXIT
-trap '[[ -n ${tmpfile-} ]] && rm -f "${tmpfile}"' SIGHUP SIGINT SIGTERM
+trap '[[ -n ${TMPFILE-} ]] && rm -f "${TMPFILE}"' SIGHUP SIGINT SIGTERM
 
-### PDF to text ###
-pdftotext ${pdfname} ${tmpfile1}
-
-### Shaping ###
-sed -e ':loop; N; $!b loop; s/\([a-z]\)-/\1/g' ${tmpfile1} > ${tmpfile2}
-sed -e ':loop; N; $!b loop; s/\([a-z]\)\n/\1 /g' ${tmpfile2} > ${tmpfile1}
-sed 's/\([a-z]\)\(\.\)/\1\2\n/g' ${tmpfile1} > ${filename}
-
-### trans ###
-echo "\\documentclass{jsarticle}\n\\\begin{document}\n" > ${filename}.tex
-trans -b -i ${filename} :${language} >> ${filename}.tex
-echo "\\end{document}" >> ${filename}.tex
-
-### LaTeX ###
-if [ ! -d LaTeX ]; then
-    mkdir LaTeX
+if [ ! -d $FOLDERNAME ]; then
+    mkdir -p $FOLDERNAME
 fi
 
-cd LaTeX
-mv ../${filename}.tex ./
+cd ${FOLDERNAME}
 
-ptex2pdf -l ${filename}.tex
+### PDF to text ###
+pdftotext ../${PDFNAME} ${TMPFILE1}
+
+### Shaping ###
+sed -e ':loop; N; $!b loop; s/\([a-z]\)-/\1/g' ${TMPFILE1} > ${TMPFILE2}
+sed -e ':loop; N; $!b loop; s/\([a-z]\)\n/\1 /g' ${TMPFILE2} > ${TMPFILE1}
+sed 's/\([a-z]\)\(\.\)/\1\2\n/g' ${TMPFILE1} > ${FILENAME}
+
+### trans ###
+echo "\\documentclass{jsarticle}\n\\\begin{document}\n" > ${FILENAME}.tex
+trans -b -i ${FILENAME} :${LANGUAGE} >> ${FILENAME}.tex
+echo "\\end{document}" >> ${FILENAME}.tex
+
+### LaTeX ###
+ptex2pdf -l ${FILENAME}.tex
 
 ### PDF display ###
-evince ${filename}.pdf
+evince ${FILENAME}.pdf
